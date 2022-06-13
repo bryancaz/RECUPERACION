a1187bba11efd01762629d01de73d002f181ab3b
bryancaz <bcalzadar@up.edu.mx>
2022-06-13 00:29:24 -0500

    replace commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-//FILTER
-//El método filter() crea un nuevo array con todos los elementos que cumplan la condición implementada por la función dada.
+//replace
+//El tipo de array en JavaScript nos proporciona el método splice() que nos ayuda a reemplazar los elementos de un array existente eliminando e insertando nuevos elementos en el índice requerido/deseado.
+
+const texto = 'Habia una vez un perro que solia jugar mucho';
+
+console.log(texto.replace('perro', 'caballo'));
 
 
-const cervezas = ['tecate', 'corona', 'tecate roja', 'heineken', 'corona negra', 'tecate azul', 'indio', 'carta blanca'];
-
-const result = cervezas.filter(cervezas => cervezas.length > 8);
-
-console.log(result);
+const animal = /perro/i;
+console.log(texto.replace(animal, 'rocky'));
