265df3ff9d7feb18a8520662490f2037f097553e
bryancaz <bcalzadar@up.edu.mx>
2022-06-12 23:36:20 -0500

    reduce commit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-//filter
-//El método filter() crea un nuevo array con todos los elementos que cumplan la condición implementada por la función dada.
+//reduce
+//El método reduce ejecuta una función de devolución de llamada "reductor" suministrada por el usuario en cada elemento de la matriz, en orden, pasando el valor de retorno del cálculo en el elemento precedente.
 
+const array1 = [1, 2, 3, 4];
 
-const cervezas = ['tecate', 'corona', 'tecate roja', 'heineken', 'corona negra', 'tecate azul', 'indio', 'carta blanca'];
+// 0 + 1 + 2 + 3 + 4
+const valorinicial = 0;
+const sumWithInitial = array1.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  valorinicial
+);
 
-const result = cervezas.filter(cervezas => cervezas.length > 8);
-
-console.log(result);
+console.log(sumWithInitial);
+// total == 10
